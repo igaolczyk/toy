073dd483001f6bf107eadd3e7a934ca52f6bd28b
imac <imac@MacBook-Air-imac.local>
2016-04-18 20:48:40 +0200

    awesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  def awesome
+  	render text: 'You are awesome!'
+  end
+
   protect_from_forgery with: :exception
 end
